a848e619c7ee42211ad341bb3e6fd56374752271
Thabet <thabet.rebhi@gmail.com>
2024-07-08 10:12:35 +0100

    update swift file

--- TestFramework/Sources/ContentsView.swift
+++ TestFramework/Sources/ContentsView.swift
@@ -1,17 +1,1 @@
 import SwiftUI
-
-public struct ContentsView: View {
-    public init() {}
-
-    public var bodys: some View {
-        Text("Hello, World!")
-            .padding()
-    }
-}
-
-
-struct ContentViews_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
